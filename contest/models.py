d78a2156f8cdd3035fd1741181a295a611b0ffc8
dsyun96 <dsyun96@naver.com>
2020-09-22 09:20:01 +0900

    remove jet theme

--- contest/models.py
+++ contest/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-
 from django.conf import settings
-from multiselectfield import MultiSelectField
 from koj.models import Problem, Submit
 
+
 # Create your models here.
-
-
 class Contest(models.Model):
 
     LANG = (('0', 'C'),
@@ -16,7 +13,6 @@ class Contest(models.Model):
 
     contest_id = models.AutoField('대회 번호', null=False, primary_key=True)
     title = models.CharField('제목', max_length=128)
-    lang = MultiSelectField(choices=LANG)
     winner = models.CharField('우승자', max_length=128, null=True, blank=True)
     start_time = models.DateTimeField('시작 시간', null=False)
     end_time = models.DateTimeField('종료 시간', null=False)
@@ -24,7 +20,6 @@ class Contest(models.Model):
     private = models.BooleanField('비공개 대회 여부', default=False)
     problem = models.ManyToManyField(Problem, related_name='contest_problems')
     participant = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='contest_participants')
-
 
     def __str__(self):
         return self.title
@@ -43,7 +38,3 @@ class ParticipantsSolved(models.Model):
 
     class Meta:
         verbose_name_plural = '참자가가 푼 문제'
-
-
-
-
