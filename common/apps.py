d11bb04a8e11b3d6c6668e541eed7ea171b22405
dsyun96 <dsyun96@naver.com>
2020-09-22 09:57:34 +0900

    reset

--- common/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommonConfig(AppConfig):
-    name = 'common'
-    verbose_name = '유저 관리'
--- koj/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KojConfig(AppConfig):
-    name = 'koj'
-    verbose_name = '문제 관리'
