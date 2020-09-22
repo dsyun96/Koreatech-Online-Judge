8e7dce3f4ffd87dc7abc4affc5e7da6ec69972e1
dsyun96 <dsyun96@naver.com>
2020-09-22 10:51:16 +0900

    common/forms PEP 8

--- common/forms.py
+++ common/forms.py
@@ -8,15 +8,16 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'password1', 'password2', 'email')
         widgets = {
-            'username' : forms.TextInput(attrs={'class': 'form-control item'})
+            'username': forms.TextInput(attrs={'class': 'form-control item'})
         }
         labels = {
-            'username' : 'ID'
+            'username': 'ID'
         }
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control item'}), label= 'Passwrod')
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control item'}), label= 'Confirm Password')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control item'}), label='Passwrod')
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control item'}), label='Confirm Password')
     
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control item'}), label= 'E - mail')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control item'}), label='E - mail')
 
 
 class CustomUserChangeForm(UserChangeForm):
